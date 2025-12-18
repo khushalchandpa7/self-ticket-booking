6592ebc35e97b716a17c75534136272d49ecff4f
Chandpa Khushal <chandpakhushal6@gmail.com>
2025-12-18 10:23:02 +0530

    login & register page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import "./App.css";
+
 export default function App() {
-  return (
-    <>
-      <LoginForm></LoginForm>
-    </>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
